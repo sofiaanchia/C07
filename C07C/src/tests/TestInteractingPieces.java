2420753f073e426c124953d388fff7ce6918162f
SOFIA ANCHIA <SANCHIA@mines.com>
2024-02-09 18:17:42 -0700

    fixed flower interaction to +3

--- C07C/src/tests/TestInteractingPieces.java
+++ C07C/src/tests/TestInteractingPieces.java
@@ -2,6 +2,8 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Test;
+
 import gameEngine.Drawable;
 import gameEngine.GameEngine;
 import gameEngine.InteractionResult;
@@ -11,27 +13,28 @@ import levelPieces.Monkey;
 import levelPieces.Sock;
 
 public class TestInteractingPieces {
-    public void testSockPoints() {
+    @Test 
+	public void testSockPoints() {
 	    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
 	    Sock sock = new Sock();
 	    gameBoard[sock.getLocation()] = sock;
 	    assertEquals(InteractionResult.GET_POINT, sock.interact(gameBoard, sock.getLocation()));
 	}
-	
+    @Test 
 	public void testFlowerKills() {
 	    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
 	    Flower flower = new Flower();
 	    gameBoard[flower.getLocation()] = flower;
-	    assertEquals(InteractionResult.KILL, flower.interact(gameBoard, flower.getLocation()));
+	    assertEquals(InteractionResult.KILL, flower.interact(gameBoard, flower.getLocation()+3));
 	}
-	
+    @Test 
 	public void testGumballGrantsPointOnInteraction() {
 	    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
 	    Gumball gumball = new Gumball();
 	    gameBoard[gumball.getLocation()] = gumball;
 	    assertEquals(InteractionResult.ADVANCE, gumball.interact(gameBoard, gumball.getLocation()));
 	}
-	
+    @Test 
 	public void testMonkeyHits() {
 	    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
 	    Monkey monkey = new Monkey();
