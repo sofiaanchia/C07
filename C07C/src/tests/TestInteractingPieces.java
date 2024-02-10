85e93876f213c79cc6ae2c807f54912ac5b5542e
Gordon Moore <gjmoore13@gmail.com>
2024-02-09 17:37:55 -0700

    did interactions for sock and flower

--- C07C/src/tests/TestInteractingPieces.java
+++ C07C/src/tests/TestInteractingPieces.java
@@ -1,5 +1,25 @@
 package tests;
 
-public class TestInteractingPieces {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+import levelPieces.Flower;
+import levelPieces.Sock;
+
+public class TestInteractingPieces {
+    public void testSockPoints() {
+	    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+	    Sock sock = new Sock();
+	    gameBoard[sock.getLocation()] = sock;
+	    assertEquals(InteractionResult.GET_POINT, sock.interact(gameBoard, sock.getLocation()));
+	}
+	
+	public void testFlowerKills() {
+	    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+	    Flower flower = new Flower();
+	    gameBoard[flower.getLocation()] = flower;
+	    assertEquals(InteractionResult.KILL, flower.interact(gameBoard, flower.getLocation()));
+	}
 }
--- C07C/src/tests/TestMovingPieces.java
+++ C07C/src/tests/TestMovingPieces.java
@@ -1,5 +1,14 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+import levelPieces.Flower;
+import levelPieces.Gumball;
+import levelPieces.Sock;
+
 public class TestMovingPieces {
 
 }
